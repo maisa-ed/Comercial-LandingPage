48124c539ab66604e743cd51909835bb1fa4e782
maisa-ed <maisaeduarda080@gmail.com>
2024-01-30 15:07:51 -0300

    animacoes aos

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-AOS.init()
+document.addEventListener('DOMContentLoaded', function () {
+  // Inicialização do AOS
+  AOS.init()
+
+  // Configuração do Typed.js
+  var options = {
+    strings: ['ENTREGAS RÁPIDAS, SOLUÇÕES ÁGEIS'],
+    typeSpeed: 50,
+    loop: false,
+  }
+
+  // Inicialização do Typed.js
+  var typed = new Typed('#typed-text', options)
+})
